8e636768643ebd60adf831e2d2bf251e939d8e99
printf100 <mickey0502@naver.com>
2020-10-04 15:34:03 +0900

    20201004 first commit

---
+++ demo/src/test/java/com/test/yoddang/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.yoddang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
